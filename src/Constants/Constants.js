033d85c1eab435fd8697f2a646e74dae3dc06f94
Rajath kalarikkal <60771074+rajathlu@users.noreply.github.com>
2023-01-20 22:38:39 +0530

    new change

--- src/Constants/Constants.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl ='https://www.omdbapi.com/?i=tt3896198&apikey=2cbb85e8';
-export const Api_key='2cbb85e8';
-export const imageUrl='https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg';
